6a8c7c9a19bb6abac210527d4067e13b92b15471
RyanCode <ryferreira1@outlook.es>
2020-07-06 14:21:38 -0300

    Implementando Bootstrap

--- main.js
+++ main.js
@@ -1,52 +1,60 @@
 (function(){
 	// Variables
 	var lista = document.getElementById("lista"),
-	tareaInput = document.getElementById("tareaInput"),
-	btnNuevaTarea = document.getElementById("btn-agregar");
-
+		tareaInput = document.getElementById("tareaInput"),
+		btnNuevaTarea = document.getElementById("btn-agregar");
+ 
 	// Funciones
 	var agregarTarea = function(){
 		var tarea = tareaInput.value,
 			nuevaTarea = document.createElement("li"),
 			enlace = document.createElement("a"),
 			contenido = document.createTextNode(tarea);
-
-			if (tarea === ""){
-				tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
-				tareaInput.className = "error";
-				return false;
-			}
-			// Agregamos el contenido al enlace
-			enlace.appendChild(contenido);
-			// Le establecemos un atributo href
-			enlace.setAttribute("href", "#");
-			// Agregamos el enlace (a) a la nueva tarea (li)
-			nuevaTarea.appendChild(enlace);
-			// Agregamos la nueva tarea a la lista
-			lista.appendChild(nuevaTarea);
-
-			tareaInput.value = "";
+ 
+		if (tarea === "") {
+			tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
+			tareaInput.setAttribute = "required";
+			tareaInput.className = "border border-danger form-control";
+			return false;
+		}
+ 
+		// Agregamos el contenido al enlace
+		enlace.appendChild(contenido);
+		// Le establecemos un atributo href
+		enlace.setAttribute("href", "#");
+		// Agrergamos el enlace (a) a la nueva tarea (li)
+		nuevaTarea.appendChild(enlace);
+		// Agregamos la nueva tarea a la lista
+		lista.appendChild(nuevaTarea);
+ 
+		tareaInput.value = "";
+ 
+		for (var i = 0; i <= lista.children.length -1; i++) {
+			lista.children[i].addEventListener("click", function(){
+				this.parentNode.removeChild(this);
+			});
+		}
+ 
 	};
 	var comprobarInput = function(){
-		
+		tareaInput.className = "form-control";
+		teareaInput.setAttribute("placeholder", "Agrega tu tarea");
 	};
-	var eliminarTarea = function(){
-		alert("a");
+ 
+	var eleminarTarea = function(){
+		this.parentNode.removeChild(this);
 	};
-
+ 
 	// Eventos
+ 
+		// Agregar Tarea
 	btnNuevaTarea.addEventListener("click", agregarTarea);
-
-	// Comprobar Imput
+ 
+		// Comprobar Input
 	tareaInput.addEventListener("click", comprobarInput);
-	for(var i = 0; i <= lista.children.length -1; i++){
-		lista.children[i].addEventListener("click", function (){
-			this.parentNode.removeChild(this);
-		});
-	};
-	
-	// Borrando elemento de la página
-	for(var i = 0; i <= lista.children.length -1; i++){
-	lista.children[i].addEventListener("click", eliminarTarea);
-	};
+ 
+		// Borrando Elementos de la lista
+	for (var i = 0; i <= lista.children.length -1; i++) {
+		lista.children[i].addEventListener("click", eleminarTarea);
+	}
 }());
