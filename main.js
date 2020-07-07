7dc6bdca9a33115e84b89b4c4cf22f0eddc355ed
RyanCode <ryferreira1@outlook.es>
2020-07-07 11:28:43 -0300

    acabado-estilos-css

--- main.js
+++ main.js
@@ -3,13 +3,14 @@
 	var lista = document.getElementById("lista"),
 		tareaInput = document.getElementById("tareaInput"),
 		btnNuevaTarea = document.getElementById("btn-agregar");
- 
+
 	// Funciones
 	var agregarTarea = function(){
 		var tarea = tareaInput.value,
 			nuevaTarea = document.createElement("li"),
 			enlace = document.createElement("a"),
 			contenido = document.createTextNode(tarea);
+
  
 		if (tarea === "") {
 			tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
@@ -45,15 +46,15 @@
 	};
  
 	// Eventos
+
+	// Agregar Tarea
+		btnNuevaTarea.addEventListener("click", agregarTarea);
  
-		// Agregar Tarea
-	btnNuevaTarea.addEventListener("click", agregarTarea);
+	// Comprobar Input
+		tareaInput.addEventListener("click", comprobarInput);
  
-		// Comprobar Input
-	tareaInput.addEventListener("click", comprobarInput);
- 
-		// Borrando Elementos de la lista
-	for (var i = 0; i <= lista.children.length -1; i++) {
+	// Borrando Elementos de la lista
+		for (var i = 0; i <= lista.children.length -1; i++) {
 		lista.children[i].addEventListener("click", eleminarTarea);
-	}
+		}
 }());
